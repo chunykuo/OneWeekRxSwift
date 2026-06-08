afa19ca478d0a3ba00dcf2c3dd2b2dfbc34df6f3
David Kuo <h124079274@msn.com>
2026-06-08 14:41:08 +0800

    feat: add day 2 contents

--- OneWeekRxSwift/ViewController.swift
+++ OneWeekRxSwift/ViewController.swift
@@ -13,13 +13,19 @@ import RxSwift
 // DisposeBag：管理訂閱生命週期
 let disposeBag = DisposeBag()
 
-// Observable：事件來源
-let observable = Observable.of("Apple", "Banana", "Cherry")
-
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+//        day1()
+        
+        day2()
+    }
+
+    private func day1() {
+        // Observable：事件來源
+        let observable = Observable.of("Apple", "Banana", "Cherry")
         
         // Subscribe：監聽事件
         observable
@@ -30,7 +36,19 @@ class ViewController: UIViewController {
             })
             .disposed(by: disposeBag)
     }
-
+    
+    private func day2() {
+        let searchText = Observable.of("", "a", "ap", "app", "apple")
+        
+        searchText
+            .filter { !$0.isEmpty }
+            .filter { $0.count >= 3 }
+            .map { $0.uppercased() }
+            .subscribe(onNext: { keyword in
+                print("搜尋關鍵字：\(keyword)")
+            })
+            .disposed(by: disposeBag)
+    }
 
 }
 
